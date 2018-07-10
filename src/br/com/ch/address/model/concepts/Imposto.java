ae61ded56259861fd52d335d692ebe444d792729
MiKalec <mikael12-1998@hotmail.com>
2018-07-09 21:07:51 -0300

    Subindo algumas classes

--- src/br/com/ch/address/model/concepts/Imposto.java
+++ src/br/com/ch/address/model/concepts/Imposto.java
@@ -1,4 +1,13 @@
 package br.com.ch.address.model.concepts;
 
 public class Imposto {
+    private Double valorPorcentagem;
+
+    public Double getValorPorcentagem() {
+        return valorPorcentagem;
+    }
+
+    public void setValorPorcentagem(Double valorPorcentagem) {
+        this.valorPorcentagem = valorPorcentagem;
+    }
 }
--- src/br/com/ch/address/model/concepts/Producao.java
+++ src/br/com/ch/address/model/concepts/Producao.java
@@ -3,7 +3,35 @@ package br.com.ch.address.model.concepts;
 import br.com.ch.address.interfaces.Concept;
 
 public class Producao implements Concept {
+    private Double custo;
+    private Double valor;
+    private Imposto imposto;
 
+    public Double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(Double custoMateriaPrima, Imposto imposto, Double custoMaoDeObra) {
+        this.custo = (custoMateriaPrima + custoMaoDeObra) * imposto.getValorPorcentagem();
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double lucroEsperadoPorcentagem, Double custo, Double despesa) {
+        lucroEsperadoPorcentagem = lucroEsperadoPorcentagem /100;
+        lucroEsperadoPorcentagem = lucroEsperadoPorcentagem + 1.0;
+        this.valor = (custo + despesa) * lucroEsperadoPorcentagem;
+    }
+
+    public Imposto getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(Imposto imposto) {
+        this.imposto = imposto;
+    }
 
     @Override
     public void aumenta() {
@@ -12,6 +40,22 @@ public class Producao implements Concept {
 
     @Override
     public void reduz() {
+
+    }
+
+    public void aumentaCusto(){
+
+    }
+
+    public void reduzCusto(){
+
+    }
+
+    public void aumentaValor(){
+
+    }
+
+    public void reduzValor(){
 
     }
 }
