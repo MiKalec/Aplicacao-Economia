4ec0f3ce1cf9729022721774a8ffd9194b34c15b
ax300 <and.f.c@hotmail.com>
2018-07-12 00:21:35 -0300

    Meu cu

--- src/br/com/ch/address/model/concepts/PNB.java
+++ src/br/com/ch/address/model/concepts/PNB.java
@@ -1,6 +1,9 @@
 package br.com.ch.address.model.concepts;
 
 public class PNB {
+
+    public final String definicao = "PNB remete a soma de valor de produtos, bens e serviços sociais do país e levando em conta somente o que é"
+            + "produzido dentro deste território nacional excluindo então importações e serviços estrangeiros durante um certo período.";
     private double valor;
     //outros atributos e métodos
 
