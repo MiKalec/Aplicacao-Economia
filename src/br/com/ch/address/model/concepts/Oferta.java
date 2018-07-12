47b66f9184a5ed0de9ad6492e5c994266878b13c
ax300 <and.f.c@hotmail.com>
2018-07-11 23:10:15 -0300

    Subindo algumas classes

--- src/br/com/ch/address/model/concepts/Oferta.java
+++ src/br/com/ch/address/model/concepts/Oferta.java
@@ -2,7 +2,8 @@ package br.com.ch.address.model.concepts;
 
 public class Oferta {
     Demanda demanda = new Demanda();
-    public final String definicao = "Oferta eh a quantidade de um produto ou servico disponivel para compra. A oferta varia nao soh com a demanda, mas tambem com fatores que influenciam o preco dela. Politicas governamentais, condicoes naturais e o proprio preco do produto podem alterar a curva de demanda de 1 ou mais produtos.  "
+    public final String definicao = "Oferta eh a quantidade de um produto ou servico disponivel para compra. A oferta varia nao soh com a demanda, mas tambem com fatores que influenciam o preco dela. Politicas governamentais, condicoes naturais e o proprio preco do produto podem alterar a curva de demanda de 1 ou mais produtos.  ";
+
     public void aumenta(Double porcentagem) {
         //implementar lógica de aumento de oferta
     }
@@ -10,13 +11,16 @@ public class Oferta {
     public void reduz(Double porcentagem) {
         //implementar lógica de reducao de oferta
     }
-    public void getPrecoOferta(){
+
+    public void getPrecoOferta() {
 
     }
-    public void getImpostoOferta(){
+
+    public void getImpostoOferta() {
 
     }
-    public void getNaturalCondOferta(){ //condicoes naturais fav para producao
+
+    public void getNaturalCondOferta() { //condicoes naturais fav para producao
 
     }
 }
